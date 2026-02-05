42cc017344e4c7c9d33acda05e5ef7ce62cd1863
Avishka777 <a.rathnakumara777@gmail.com>
2026-02-05 19:33:02 +0530

    Fixed Minor Issues

--- component/auth/ChangePassword.tsx
+++ component/auth/ChangePassword.tsx
@@ -27,7 +27,6 @@ import { useFocusEffect } from "@react-navigation/native";
 import NetInfo from "@react-native-community/netinfo";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-
 type ChangePasswordNavigationProp = StackNavigationProp<
   RootStackParamList,
   "ChangePassword"
@@ -37,17 +36,15 @@ interface ChangePasswordProps {
   navigation: ChangePasswordNavigationProp;
 }
 
-const ChangePassword: React.FC<ChangePasswordProps> = ({
-  navigation,
-}) => {
+const ChangePassword: React.FC<ChangePasswordProps> = ({ navigation }) => {
   const [currentPassword, setCurrentPassword] = useState("");
   const [newPassword, setNewPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [secureCurrent, setSecureCurrent] = useState(true);
   const [secureNew, setSecureNew] = useState(true);
   const [secureConfirm, setSecureConfirm] = useState(true);
-   const [passwordUpdate, setPasswordUpdate] = useState<number | null>(null); 
-   console.log(passwordUpdate)
+  const [passwordUpdate, setPasswordUpdate] = useState<number | null>(null);
+  console.log(passwordUpdate);
   const { t } = useTranslation();
 
   const validatePassword = () => {
@@ -65,7 +62,10 @@ const ChangePassword: React.FC<ChangePasswordProps> = ({
 
     // Check for at least 1 uppercase letter
     if (!/[A-Z]/.test(newPassword)) {
-      Alert.alert(t("Error.error"), t("Error.Your password must contain a minimum"));
+      Alert.alert(
+        t("Error.error"),
+        t("Error.Your password must contain a minimum"),
+      );
       return false;
     }
 
@@ -77,13 +77,19 @@ const ChangePassword: React.FC<ChangePasswordProps> = ({
 
     // Check for at least 1 special character
     if (!/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(newPassword)) {
-       Alert.alert(t("Error.error"), t("Error.Your password must contain a minimum"));
+      Alert.alert(
+        t("Error.error"),
+        t("Error.Your password must contain a minimum"),
+      );
       return false;
     }
 
     // Check if new password and confirm password match
     if (newPassword !== confirmPassword) {
-      Alert.alert(t("Error.error"), 'New password and confirm password do not match');
+      Alert.alert(
+        t("Error.error"),
+        "New password and confirm password do not match",
+      );
       return false;
     }
 
@@ -94,9 +100,9 @@ const ChangePassword: React.FC<ChangePasswordProps> = ({
     const fetchEmpid = async () => {
       try {
         const response = await axios.get(
-          `${environment.API_BASE_URL}api/collection-officer/empid/`
+          `${environment.API_BASE_URL}api/collection-officer/empid/`,
         );
-      //  console.log("Empid response:", response.data);
+        //  console.log("Empid response:", response.data);
       } catch (error) {
         Alert.alert(t("Error.error"), t("Error.Failed to fetch empid."));
       }
@@ -104,34 +110,33 @@ const ChangePassword: React.FC<ChangePasswordProps> = ({
   }, []);
 
   const handleChangePassword = async () => {
-    
-        Keyboard.dismiss();
+    Keyboard.dismiss();
     // Validate inputs before proceeding
     if (!validatePassword()) {
       return;
     }
 
-     const netState = await NetInfo.fetch();
-      if (!netState.isConnected) {
-    return; 
-  }
+    const netState = await NetInfo.fetch();
+    if (!netState.isConnected) {
+      return;
+    }
 
     try {
-         const token = await AsyncStorage.getItem('token');
-  const response = await axios.post(
-    `${environment.API_BASE_URL}api/collection-officer/change-password`,
-    {
-      currentPassword,
-      newPassword,
-    }, 
-    {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    } 
-  );
+      const token = await AsyncStorage.getItem("token");
+      const response = await axios.post(
+        `${environment.API_BASE_URL}api/collection-officer/change-password`,
+        {
+          currentPassword,
+          newPassword,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
 
-    //  console.log("Password update response:", response.data);
+      //  console.log("Password update response:", response.data);
       Alert.alert(t("Error.Success"), t("Error.Password updated successfully"));
       navigation.navigate("Login");
     } catch (error) {
@@ -148,25 +153,20 @@ const ChangePassword: React.FC<ChangePasswordProps> = ({
     }
   };
 
-    //   useFocusEffect(
-    //   useCallback(() => {
-    //     const onBackPress = () => true;
-    //     BackHandler.addEventListener("hardwareBackPress", onBackPress);
-    //       const subscription = BackHandler.addEventListener("hardwareBackPress", onBackPress);
-    //   return () => subscription.remove();
-    //   }, [])
-    // );
- const fetchOfficer = async () => {
+  const fetchOfficer = async () => {
     try {
-      const token = await AsyncStorage.getItem('token');
+      const token = await AsyncStorage.getItem("token");
 
-      const response = await axios.get(`${environment.API_BASE_URL}api/collection-officer/password-update`, {
-        headers: {
-          Authorization: `Bearer ${token}`,
+      const response = await axios.get(
+        `${environment.API_BASE_URL}api/collection-officer/password-update`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
         },
-      });
+      );
 
-         setPasswordUpdate(response.data.data.passwordUpdated);
+      setPasswordUpdate(response.data.data.passwordUpdated);
 
       console.log("update password status", response.data.data.passwordUpdated);
     } catch (error) {
@@ -178,50 +178,55 @@ const ChangePassword: React.FC<ChangePasswordProps> = ({
     fetchOfficer();
   }, []);
 
-      useFocusEffect(
+  useFocusEffect(
     useCallback(() => {
       const onBackPress = () => {
         // If passwordUpdate is 0, prevent back navigation
         if (passwordUpdate === 0) {
-          console.log("hitt")
+          console.log("hitt");
           return true; // Prevent back navigation
-          
         }
         // If passwordUpdate is 1, allow back navigation
         return false; // Allow back navigation
       };
-      
-      const subscription = BackHandler.addEventListener("hardwareBackPress", onBackPress);
-      return () => subscription.remove();
-    }, [passwordUpdate]) // Added passwordUpdate as dependency
-  );
 
+      const subscription = BackHandler.addEventListener(
+        "hardwareBackPress",
+        onBackPress,
+      );
+      return () => subscription.remove();
+    }, [passwordUpdate]), // Added passwordUpdate as dependency
+  );
 
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === "ios" ? "padding" : "height"}
       enabled
-      style={{ flex: 1}}
+      style={{ flex: 1 }}
     >
       <ScrollView
         className="flex-1 bg-white"
         style={{ paddingHorizontal: wp(6), paddingVertical: hp(2) }}
         keyboardShouldPersistTaps="handled"
       >
-       {passwordUpdate === 1 && (
-        <TouchableOpacity  onPress={() => navigation.goBack()} className="bg-[#f3f3f380] rounded-full p-2 justify-center w-10" >
-                         <AntDesign name="left" size={24} color="#000502" />
-                       </TouchableOpacity>
-       )}
-        <View className={`flex-row items-center justify-center ${
-    passwordUpdate === 1 ? "mt-[2%]" : "mt-[12%]"
-  }  space-x-[-30%] ml-[5%]`}>
-         <Image
+        {passwordUpdate === 1 && (
+          <TouchableOpacity
+            onPress={() => navigation.goBack()}
+            className="bg-[#f3f3f380] rounded-full p-2 justify-center w-10"
+          >
+            <AntDesign name="left" size={24} color="#000502" />
+          </TouchableOpacity>
+        )}
+        <View
+          className={`flex-row items-center justify-center ${
+            passwordUpdate === 1 ? "mt-[2%]" : "mt-[12%]"
+          }  space-x-[-30%] ml-[5%]`}
+        >
+          <Image
             source={require("@/assets/images/cangepassword.png")}
             resizeMode="contain"
             className="w-30 h-20"
-          /> 
-         
+          />
         </View>
 
         <View className="items-center pt-[5%]">
@@ -238,13 +243,12 @@ const ChangePassword: React.FC<ChangePasswordProps> = ({
             {t("ChangePassword.CurrentPassword")}
           </Text>
           <View className="flex-row items-center bg-[#F4F4F4] border border-[#F4F4F4] rounded-3xl w-[95%] h-[53px] mb-8 px-3">
-          <TextInput
-            className="flex-1 h-[40px] bg-[#F4F4F4]"
-          //  placeholder={t("ChangePassword.CurrentPassword")}
-            secureTextEntry={secureCurrent}
-            onChangeText={setCurrentPassword}
-            value={currentPassword}
-          />
+            <TextInput
+              className="flex-1 h-[40px] bg-[#F4F4F4]"
+              secureTextEntry={secureCurrent}
+              onChangeText={setCurrentPassword}
+              value={currentPassword}
+            />
             <TouchableOpacity onPress={() => setSecureCurrent(!secureCurrent)}>
               <Icon
                 name={secureCurrent ? "eye-off-outline" : "eye-outline"}
@@ -257,18 +261,15 @@ const ChangePassword: React.FC<ChangePasswordProps> = ({
           <Text className="font-normal pb-2 items-start self-start ml-4 ">
             {t("ChangePassword.NewPassword")}
           </Text>
-             <View className="flex-row items-center bg-[#F4F4F4] border border-[#F4F4F4] rounded-3xl w-[95%] h-[53px] mb-8 px-3">
+          <View className="flex-row items-center bg-[#F4F4F4] border border-[#F4F4F4] rounded-3xl w-[95%] h-[53px] mb-8 px-3">
             <TextInput
               className="flex-1 h-[40px] "
-
               secureTextEntry={secureNew}
-              // onChangeText={setNewPassword}
               value={newPassword}
-                 onChangeText={(text) => {
-  
-      const cleanText = text.replace(/\s/g, '');
-      setNewPassword(cleanText);
-    }}
+              onChangeText={(text) => {
+                const cleanText = text.replace(/\s/g, "");
+                setNewPassword(cleanText);
+              }}
             />
             <TouchableOpacity onPress={() => setSecureNew(!secureNew)}>
               <Icon
@@ -282,17 +283,14 @@ const ChangePassword: React.FC<ChangePasswordProps> = ({
           <Text className="font-normal pb-2 self-start ml-4">
             {t("ChangePassword.ConfirmNewPassword")}
           </Text>
-           <View className="flex-row items-center bg-[#F4F4F4] border border-[#F4F4F4] rounded-3xl w-[95%] h-[53px] mb-8 px-3">
+          <View className="flex-row items-center bg-[#F4F4F4] border border-[#F4F4F4] rounded-3xl w-[95%] h-[53px] mb-8 px-3">
             <TextInput
               className="flex-1 h-[40px] bg-[#F4F4F4]"
-          
               secureTextEntry={secureConfirm}
-              // onChangeText={setConfirmPassword}
-                        onChangeText={(text) => {
-   
-      const cleanText = text.replace(/\s/g, '');
-      setConfirmPassword(cleanText);
-    }}
+              onChangeText={(text) => {
+                const cleanText = text.replace(/\s/g, "");
+                setConfirmPassword(cleanText);
+              }}
               value={confirmPassword}
             />
             <TouchableOpacity onPress={() => setSecureConfirm(!secureConfirm)}>
@@ -305,19 +303,16 @@ const ChangePassword: React.FC<ChangePasswordProps> = ({
           </View>
         </View>
 
-     
         <View className="items-center justify-center pt-7 gap-y-5 mb-20">
-  <TouchableOpacity
-    className="bg-[#000000] w-[95%] p-3 rounded-full items-center justify-center"
-    onPress={handleChangePassword}
-  >
-
-    <Text className="text-xl font-light text-white">
-      {t("ChangePassword.Next")}
-    </Text>
-  </TouchableOpacity>
-</View>
-
+          <TouchableOpacity
+            className="bg-[#000000] w-[95%] p-3 rounded-full items-center justify-center"
+            onPress={handleChangePassword}
+          >
+            <Text className="text-xl font-light text-white">
+              {t("ChangePassword.Next")}
+            </Text>
+          </TouchableOpacity>
+        </View>
       </ScrollView>
     </KeyboardAvoidingView>
   );
